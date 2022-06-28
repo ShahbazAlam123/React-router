b3f03cab0d75c5b3346dcb3baa4feac72e922c1e
MDSHAHBAZ ALAM <mdshahbaz.alam@mphasis.com>
2022-06-28 11:58:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services';
+import Contacts from './pages/Contacts';
+import Navbar from './component/Navbar';
+ //import {mockFile} from './component/Mock'
+
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* {
+       mockFile.map((item,i)=>{
+        return(
+          <Routes>
+            <Route key={i} path={item.path} element={<item.name/>}/>
+        </Routes>
+        )
+       })
+      } */}
+    
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/contacts' element={<Contacts/>}/>
+      </Routes>
     </div>
+    </>
   );
 }
 
